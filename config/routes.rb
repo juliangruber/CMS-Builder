72d07c87d6fa8161387965aec2b1646442e25977
Julian Gruber <julian@juliangruber.com>
2012-02-06 15:51:40 +0100

    added admin to its own namespace

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ Cms::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   #match ':controller(/:action(/:id))(.:format)'
 
-  resources :relationships
-  resources :content_types
+  namespace :structure do
+    resources :content_types
+  end
 end
